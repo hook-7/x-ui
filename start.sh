b9a3b976abf5b53f09c118b4fa94484e120d2749
杨超 <98072436+hook-7@users.noreply.github.com>
2023-06-11 11:58:25 +0000

    add warp

--- start.sh
+++ start.sh
@@ -1,9 +1,9 @@
 # chmod +x ./frp/frpc
 # docker-compose up -d
 # ./frp/frpc -c ./frp/frpc.ini &
-# nginx 
+nginx 
 warp-svc 
-# &
 # warp-cli register
 # warp-cli set-mode proxy
 # warp-cli connect
+# curl -x http://127.0.0.1:40000 https://www.cloudflare.com/cdn-cgi/trace/
